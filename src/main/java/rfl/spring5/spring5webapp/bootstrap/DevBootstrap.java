3fac7d7ad8b8cfd57095586d1fd0f5e509608297
Ronald Lens <ronald@ronaldlens.com>
2018-02-07 11:08:47 +0100

    add bootstrap class to insert data

---
+++ src/main/java/rfl/spring5/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,45 @@
+package rfl.spring5.spring5webapp.bootstrap;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import rfl.spring5.spring5webapp.model.Author;
+import rfl.spring5.spring5webapp.model.Book;
+import rfl.spring5.spring5webapp.repositories.AuthorRepository;
+import rfl.spring5.spring5webapp.repositories.BookRepository;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    private void initData() {
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE developement without EJB", "1235", "Wrox");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+}
